f4275519c1dc082778b798ca096cc69a8aadc057
bugra <bugra@DESKTOP-F81L0OF>
2022-08-09 20:32:33 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    internal class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Tebrikler.. Eklendi : " + musteri.Adi + musteri.Soyadi);
+        }
+        public void Liste(Musteri musteri)
+        {
+            Console.WriteLine("Adı : " + musteri.Adi);
+            Console.WriteLine("Soyadı : " + musteri.Soyadi);
+        }
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("Müşteriyi Sil : " + musteri.Adi + musteri.Soyadi);
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,44 @@
+﻿using ClassMetotDemo;
+using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------Müşteri BİLGİLERİ------");
+            Musteri musteri1 = new Musteri();
+            musteri1.Adi = "Ahmet";
+            musteri1.Soyadi = "Saygın";
+
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Adi = "Kamil";
+            musteri2.Soyadi = "Zorlu";
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2 };
+
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Adi);
+                Console.WriteLine(musteri.Soyadi);
+            }
+            Console.WriteLine("-------Müşteri Ekleme------");
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+
+            Console.WriteLine("------Müşteri Listeleme------");
+            musteriManager.Liste(musteri1);
+            musteriManager.Liste(musteri2);
+
+
+            Console.WriteLine("-----Müşteri Silme-----");
+            musteriManager.Sil(musteri1);
+            musteriManager.Sil(musteri2);
+
+
+        }
+    }
+}
